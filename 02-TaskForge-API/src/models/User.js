65eff69ad6f75dafbf8a4d116713dbcf3264dc50
sh1v-max <singhshiv0427@gmail.com>
2026-05-13 10:45:38 +0530

    hashing password before saving the user

--- 02-TaskForge-API/src/models/User.js
+++ 02-TaskForge-API/src/models/User.js
@@ -31,12 +31,6 @@ const userSchema = new mongoose.Schema(
 )
 
 // now we will hash the password before saving the user to the database
-// userSchema.pre('save', async function () {
-//   if (!this.isModified('password')) return
-
-//   const salt = await bcrypt.genSalt(10)
-//   this.password = await bcrypt.hash(this.password, salt)
-// })
 userSchema.pre('save', async function () {
   if (!this.isModified('password')) return
 
