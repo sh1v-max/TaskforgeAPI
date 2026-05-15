3625c3624575b73049cd02b93730b0319b975c06
sh1v-max <singhshiv0427@gmail.com>
2026-05-15 19:33:05 +0530

    auth router done

--- 02-TaskForge-API/src/routes/auth.router.js
+++ 02-TaskForge-API/src/routes/auth.router.js
@@ -1,3 +1,11 @@
+// this router handles user registration and login
+// it defines two routes: /register and /login
+// for the /register route, it uses the validate middleware to validate the request body against the registerSchema before calling the register controller function
+// for the /login route, it uses the validate middleware to validate the request body against the loginSchema before calling the login controller function
+
+// validate middleware is a custom middleware that validates the request body against the provided schema
+// registerSchema and  loginSchema are zod schemas that define the structure and validation rules for the registration and login data respectively 
+
 import express from 'express'
 import { register } from '../controllers/auth.controller.js'
 import { validate } from '../middleware/validate.js'
@@ -11,11 +19,3 @@ router.post('/register', validate(registerSchema), register)
 router.post('/login', validate(loginSchema), login)
 
 export default router
-
-// this router handles user registration and login
-// it defines two routes: /register and /login
-// for the /register route, it uses the validate middleware to validate the request body against the registerSchema before calling the register controller function
-// for the /login route, it uses the validate middleware to validate the request body against the loginSchema before calling the login controller function
-
-// validate middleware is a custom middleware that validates the request body against the provided schema
-// registerSchema and loginSchema are zod schemas that define the structure and validation rules for the registration and login data respectively
