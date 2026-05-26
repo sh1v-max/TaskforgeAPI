84eb0ac6d0b3a892635b2b8afe186de620d789e0
sh1v-max <singhshiv0427@gmail.com>
2026-05-26 19:58:05 +0530

    database connection

--- 02-TaskForge-API/src/utils/db.js
+++ 02-TaskForge-API/src/utils/db.js
@@ -1,8 +1,17 @@
+// this file connects node.js application to mongodb
+// and export a reusable database connection
+
 import mongoose from 'mongoose'
 
 const connectDB = async () => {
+  // creates an async function responsible for connecting to mongoDB
   try {
     await mongoose.connect(process.env.MONGO_URI)
+    // this is actual database connection
+    // connect to mongoDB atlas
+    // creates connection pool
+    // keeps the connection alive
+    // allows models to start querying the database
     console.log('connected to mongoDB')
   } catch (error) {
     console.error('mongoDB connection failed:', error)
